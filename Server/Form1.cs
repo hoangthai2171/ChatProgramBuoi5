8584fb61be7e3907d9efdd4dd15c73d60ecf6f28
hoangthai2171 <hoangthai2171@gmail.com>
2018-04-10 18:07:58 +0700

    Chat Program Buoi 5

---
+++ Server/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        Socket server, client;
+        byte[] data;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Any, 1234);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipServer);
+            server.Listen(5);
+            client = server.Accept();
+            server.BeginAccept(new AsyncCallback(AcceptClient), server);
+            
+            IPEndPoint ip = (IPEndPoint)client.RemoteEndPoint;
+            textBox2.Text = ip.Address.ToString();
+            data = new byte[1024];
+            client.Receive(data);
+            listBox1.Items.Add(Encoding.ASCII.GetString(data));
+            
+        }
+
+        private void AcceptClient(IAsyncResult i)
+        {
+            client = ((Socket)i.AsyncState).EndAccept(i);
+            client.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(ReceiveData), client);
+        }
+
+        private void ReceiveData(IAsyncResult i)
+        {
+            ((Socket)i.AsyncState).EndReceive(i);
+            this.Invoke((MethodInvoker)(() => listBox1.Items.Add("Client: " + Encoding.ASCII.GetString(data))));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            listBox1.Items.Add(text);
+            textBox1.Text = "";
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(text);
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            listBox1.Items.Add(Encoding.ASCII.GetString(data));
+        }
+    }
+}
